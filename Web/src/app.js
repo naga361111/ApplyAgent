71cccb37f86fad854a29c4d4ba294399fa411cf8
Dongyeon Chn <148536087+naga361111@users.noreply.github.com>
2025-11-09 03:15:51 +0900

    file structure change

---
+++ Web/src/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const submitButton = document.getElementById('submitBtn');
+
+    submitButton.addEventListener('click', () => {
+        const userName = document.getElementById('userName').value;
+        const age = document.getElementById('age').value;
+        const gender = document.getElementById('gender').value;
+        const nationalId = document.getElementById('nationalId').value;
+        const residence = document.getElementById('residence').value;
+
+        const formData = {
+            name: userName,
+            age: age,
+            gender: gender,
+            nationalId: nationalId,
+            residence: residence,
+        };
+
+        fetch('https://applyagent.onrender.com/submit-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('서버 응답:', data);
+                alert('제출 성공!');
+            })
+            .catch(error => {
+                console.error('전송 실패:', error);
+                alert('제출 실패.');
+            });
+    });
+});
